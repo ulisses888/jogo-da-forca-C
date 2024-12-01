56283ceeba7bbe8268f97afe3974815e5f69665b
ulisses888 <ulisses.jr2002@gmail.com>
2024-11-30 21:38:57 -0300

    Função que gera as palavras

--- main.c
+++ main.c
@@ -1,13 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-int palavra();
+int palavra(char *palavraEscolhida);
+
+
 
 int main(){
 srand(time(0));
 
-palavra();
+char palavraEscolhida[100];
+palavra(palavraEscolhida);
+
+printf("%s",palavraEscolhida);
 
 return 0;
 }
@@ -17,9 +23,24 @@ return 0;
 
 
 
-int palavra(){
-    int numeroPalavra = rand() % 100;
-    printf("%d",numeroPalavra);
+int palavra(char *palavraEscolhida){
+    int numeroPalavra = rand() % 100 + 1;
+    int linhaAtual = 0;
+    
+    FILE *arquivo;
+    arquivo = fopen("palavras.txt", "r");
+    if (arquivo == NULL){
+        printf("Erro ao encontrar o arquivo com as palavras");
+        return 1;
+    }
+
+    for(linhaAtual;linhaAtual<numeroPalavra;linhaAtual++){
+        fgets(palavraEscolhida, 100, arquivo);
+    }
+
+            
+    fclose(arquivo);
+    
     return 0;
 
 }
